622bf67748c2b24a6747a9e7fa5b81164b6e053f
debbiect246 <deb246@btinternet.com>
2019-06-23 08:54:09 +0100

    added heroku app to allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get(
-    'localhost', '127.0.0.1'), 'django-bootcamp-app']
+    'localhost', '127.0.0.1'), 'django-bootcamp-app.herokuapp.com']
 
 
 # Application definition
